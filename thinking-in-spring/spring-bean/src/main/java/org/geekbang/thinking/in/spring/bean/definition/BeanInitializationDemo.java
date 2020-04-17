a4ebac3876bf5b139cb5876ab301167d92fb2f14
MollyQI3104 <moollyqi@outlook.com>
2020-04-18 01:14:10 +0900

    Bean 销毁

--- thinking-in-spring/spring-bean/src/main/java/org/geekbang/thinking/in/spring/bean/definition/BeanInitializationDemo.java
+++ thinking-in-spring/spring-bean/src/main/java/org/geekbang/thinking/in/spring/bean/definition/BeanInitializationDemo.java
@@ -12,6 +12,7 @@ import org.geekbang.thinking.in.spring.bean.factory.UserFactory;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
 
 @Configuration //Configuration class
 public class BeanInitializationDemo {
@@ -24,7 +25,11 @@ public class BeanInitializationDemo {
         applicationContext.register(BeanInitializationDemo.class);
 
         //启动应用上下文
-        applicationContext.refresh();
+        applicationContext.refresh();//非延迟的初始化在此过程中
+
+        //非延迟初始化 在 Spring 应用上下文启动完成后，被初始化 @Lazy(false) 或者不注解 默认就是非延迟
+        System.out.println("Spring 应用上下文已启动...");
+        //延迟初始化则是按需初始化，在依赖查找时触动了 @Lazy
 
         //依赖查找 UserFactory
         UserFactory userFactory = applicationContext.getBean(UserFactory.class);
@@ -33,11 +38,16 @@ public class BeanInitializationDemo {
         //3. 自定义初始化方法 @Bean(initMethod = "initUserFactory") 或者 xml配置 或者 通过java api
         //且符合优先级
 
+        System.out.println(userFactory);
+
+        System.out.println("Spring 应用上下文准备关闭...");
         //关闭应用上下文（显式）
-        applicationContext.close();
+        applicationContext.close();//这个过程中调用了销毁方法
+        System.out.println("Spring 应用上下文已关闭...");
     }
 
-    @Bean(initMethod = "initUserFactory")
+    @Bean(initMethod = "initUserFactory", destroyMethod = "doDestroy")
+    @Lazy //延迟初始化
     public UserFactory userFactory(){
         return new DefaultUserFactory();
     }
--- thinking-in-spring/spring-bean/src/main/java/org/geekbang/thinking/in/spring/bean/factory/DefaultUserFactory.java
+++ thinking-in-spring/spring-bean/src/main/java/org/geekbang/thinking/in/spring/bean/factory/DefaultUserFactory.java
@@ -6,11 +6,13 @@ package org.geekbang.thinking.in.spring.bean.factory;
  * @date 2020/4/17
  */
 
+import org.springframework.beans.factory.DisposableBean;
 import org.springframework.beans.factory.InitializingBean;
 
 import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
 
-public class DefaultUserFactory implements UserFactory, InitializingBean {
+public class DefaultUserFactory implements UserFactory, InitializingBean, DisposableBean {
 
     //1.基于 @PostConstruct 注解初始化
     @PostConstruct
@@ -27,4 +29,23 @@ public class DefaultUserFactory implements UserFactory, InitializingBean {
     public void afterPropertiesSet() throws Exception {
         System.out.println("InitializingBean#afterPropertiesSet(): UserFactory 初始化中...");
     }
+
+    //以下是销毁方法
+
+    @PreDestroy
+    public void preDestroy(){
+        System.out.println("@PreDestroy UserFactory 销毁中...");
+    }
+
+    //必须重写的 DisposableBean 中的方法
+    @Override
+    public void destroy() throws Exception {
+        System.out.println("DisposableBean#destroy(): UserFactory 销毁中...");
+    }
+
+    public void doDestroy(){
+        System.out.println("自定义销毁方法 doDestroy(): UserFactory 销毁中...");
+    }
+
+
 }
